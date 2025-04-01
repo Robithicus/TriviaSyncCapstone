2c48adbdc5329482e4e2a28d58d4298e792ad1cc
bunnycou <noahjcou@gmail.com>
2025-04-01 15:31:21 -0400

    updated questiondata, sessiondata

---
+++ express/questionData.js
@@ -0,0 +1,8 @@
+export class Question {
+    constructor(question, answer, choices, category) {
+        this.question = question
+        this.answer = answer
+        this.choices = choices
+        this.category = category
+    }
+}
--- express/sessionData.js
+++ express/sessionData.js
@@ -5,4 +5,43 @@ export class Session {
         this.id = id
         this.wsc = null
     }
+
+    getSessionData() {
+        return(publicData(this.id, this.questions))
+    }
+}
+
+class publicData {
+    constructor (sessionId, questions) {
+        this.sessionId = sessionId
+        this.questions = questionsRandom(questions)
+    }
+
+    questionsRandom(questions) {
+        publicQuestions = []
+        questions.forEach(question => {
+            publicQuestions.push(new QuestionPublic(question.question, question.answer, question.choices, question.category))
+        });
+    }
+}
+
+class QuestionPublic {
+    constructor(question, answer, choices, category) {
+        this.question = question
+        this.category = category
+        this.choices = randomChoices(answer, choices)
+    }
+
+    randomChoices(answer, choices) {
+        options = [answer, choices[0], choices[1], choices[2]]
+        randoms = []
+        
+        while (options.length > 0) {
+            randomnum = Math.floor(Math.random()*options.length)
+            randoms.push(options[randomnum])
+            options.splice(randomnum, 1)
+        }
+
+        return randoms
+    }
 }
